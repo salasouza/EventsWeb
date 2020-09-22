078122d9f047005fe1ea40c5c1fba77d71b6aef8
Salatiel Souza <sal.souzasa@gmail.com>
2020-09-22 17:37:38 -0300

    add js

---
+++ js/script2.js
@@ -0,0 +1,11 @@
+
+const banners = document.querySelectorAll("img");
+
+for(const banner of banners) {
+ banner.addEventListener("click", callback);
+}
+
+function callback() {
+    const src = this.getAttribute("class");
+    alert("O banner clicado foi - TESTE A: " + src);
+  }
